6410b32fab1c73b9c8cec4ce001605dd3f2a4a0b
wutsi <herve.tchepannou@wutsi.com>
2022-05-13 10:30:52 -0400

    add source

--- src/main/kotlin/com/wutsi/analytics/tracking/WutsiTrackingApi.kt
+++ src/main/kotlin/com/wutsi/analytics/tracking/WutsiTrackingApi.kt
@@ -6,13 +6,14 @@ import feign.Headers
 import feign.Param
 import feign.RequestLine
 import java.time.LocalDate
+import kotlin.Unit
 
 public interface WutsiTrackingApi {
-    @RequestLine("POST /v1/tracks")
-    @Headers(value = ["Content-Type: application/json"])
-    public fun push(request: PushTrackRequest): PushTrackResponse
+  @RequestLine("POST /v1/tracks")
+  @Headers(value=["Content-Type: application/json"])
+  public fun push(request: PushTrackRequest): PushTrackResponse
 
-    @RequestLine("GET /v1/tracks/aggregate?start-date={start-date}")
-    @Headers(value = ["Content-Type: application/json"])
-    public fun aggregate(@Param("start-date") startDate: LocalDate): Unit
+  @RequestLine("GET /v1/tracks/aggregate?start-date={start-date}")
+  @Headers(value=["Content-Type: application/json"])
+  public fun aggregate(@Param("start-date") startDate: LocalDate): Unit
 }
