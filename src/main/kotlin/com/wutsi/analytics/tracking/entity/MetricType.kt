14fdac2bac4b824e4f756afe01625f3c57c7691c
wutsi <herve.tchepannou@wutsi.com>
2022-05-20 10:16:07 -0400

    update api

--- src/main/kotlin/com/wutsi/analytics/tracking/entity/MetricType.kt
+++ src/main/kotlin/com/wutsi/analytics/tracking/entity/MetricType.kt
@@ -1,9 +1,9 @@
 package com.wutsi.analytics.tracking.entity
 
-enum class MetricType {
-    VIEW,
-    CHAT,
-    SHARE,
-    ORDER,
-    SALE,
+enum class MetricType(val eventType: EventType) {
+    VIEW(EventType.VIEW),
+    CHAT(EventType.CHAT),
+    SHARE(EventType.SHARE),
+    ORDER(EventType.CHAT),
+    SALE(EventType.ORDER),
 }
